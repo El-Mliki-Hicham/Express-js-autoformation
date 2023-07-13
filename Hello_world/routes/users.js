574045797eb9cbb596165a7c9d44b2ddd028dbe8
El-Mliki-Hicham <elmliki.hicham@gmail.com>
2023-07-13 17:06:29 +0200

    express [first application]

--- Hello_world/routes/users.js
+++ Hello_world/routes/users.js
@@ -1,10 +1,21 @@
 var expressImport = require("express")
 
 var router  = expressImport.Router()
+
 var data = [{"id":1,"name":"hicham"},{"id":2,"name":"botaina"}]
+
+router.use('/', function(req,res,next){
+    console.log('api is called')
+    next()
+})
 
 router.get("/",(req,res)=>{
     res.send('user page')
+})
+
+router.use('/', function(req,res,next){
+    console.log('api is ended')
+    next()
 })
 
 router.get("/user_data",(req,res)=>{
@@ -12,10 +23,15 @@ router.get("/user_data",(req,res)=>{
     res.send(data)
 })
 
-router.get("/user/:id",(req,res)=>{
+router.get("/user/:id([1-9])",(req,res)=>{
     var idParams =req.params.id
     var index  = data.findIndex(value=>value.id==idParams)
     res.send(data[index])
+})
+
+// wild card : when route not exist go to route not found
+/router.get('*',(req,res)=>{
+    res.send("url not found")
 })
 
 module.exports = router

c5896e53a37bb5e938682e22cf3a74f612113ea1
El-Mliki-Hicham <elmliki.hicham@gmail.com>
2023-07-13 17:59:25 +0200

    express[create template file and pass params]

--- first_app/routes/users.js
+++ first_app/routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+var data=  [{"id":1,"name":"hicham"},{"id":2,"name":"yahya"}]
+
+var i = data.findIndex(value=>value.id === 2 )
+ console.log(data[i])
+
+router.get('/table', function(req, res, next) {
+  res.render('users_table',{data:data});
+
 });
+
+router.get('/:id', function(req, res, next) {
+  var idParams=  parseInt( req.params.id)
+
+  var i = data.findIndex(value=>value.id === idParams )
+  var newData =  data[i]
+  res.render('users',{data:newData});
+
+});
+
 
 module.exports = router;

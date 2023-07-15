531534577a2c7add3f4f6764450955040a96ac63
El-Mliki-Hicham <elmliki.hicham@gmail.com>
2023-07-15 11:04:52 +0200

    express[create new project and connected db]

---
+++ first_app/connection.js
@@ -0,0 +1,6 @@
+ module.exports = {
+
+    DB_PORT :3100,
+    DB_URL :"mongodb://localhost:3049/db"
+
+ }
--- first_app/routes/index.js
+++ first_app/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
+var mongoose = require("mongoose")
+var dbUrl = require('../connection').DB_URL
+mongoose.connect(dbUrl)
+mongoose.connection.on("connected",()=>{
+  console.log('DB it connected')
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { name:"hicham", title: 'express' });
